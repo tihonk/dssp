63ffa0db8032e12ffa33103abf387ca39e8a6216
Tihon Kasko <t.kasko@itransition.com>
2021-12-08 21:59:36 +0300

    Brunch_2: Added possibility to strip and collect list

--- dssp/apps/dsspcalc/views.py
+++ dssp/apps/dsspcalc/views.py
@@ -5,9 +5,14 @@ import re
 
 @csrf_exempt
 def dsspcalc(request):
-
     if request.method == 'POST':
         pdb_file_content = request.FILES['pdbFile'].read().decode('UTF-8')
-        full_atom_coordinates = re.findall(r'ATOM(.*)\n', pdb_file_content)
+        for coordinate in re.findall(r'ATOM(.*)\n', pdb_file_content):
+            full_atom_coordinates = addTripedAtomCoordinate(coordinate)
         chain = request.POST['chain']
-    return HttpResponse(pdb_file_content)
+    return HttpResponse(full_atom_coordinates[5])
+
+
+def addTripedAtomCoordinate(coordinate, full_atom_coordinates=[]):
+    full_atom_coordinates.append(coordinate.strip())
+    return full_atom_coordinates
